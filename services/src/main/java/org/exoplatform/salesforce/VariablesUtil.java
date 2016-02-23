c2a469a8a330ca593ae378850460c5670a8780d0
Bechir <obechir@exoplatform.com>
2016-02-23 17:56:16 +0100

    Update variables Utils with correct values

--- services/src/main/java/org/exoplatform/salesforce/VariablesUtil.java
+++ services/src/main/java/org/exoplatform/salesforce/VariablesUtil.java
@@ -7,7 +7,7 @@ public interface VariablesUtil {
 
     static final String CLIENT_ID = "oauth.salesforce.clientId";
 
-    static final String REDIRECT_URI = "oauth.salesforce.clientSecret";
+    static final String REDIRECT_URI = "oauth.salesforce.redirectUri";
 
-    static final String CLIENT_SECRET = "oauth.salesforce.redirectUri";
+    static final String CLIENT_SECRET = "oauth.salesforce.clientSecret";
 }
